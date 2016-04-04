2607f2725a99a2a4cf792ee306266a75841578e8
oliverlew <oliver_lew@outlook.com>
2016-04-04 19:43:24 +0800

    add output processing of ellipse orbit

--- homework/chap2/phasegraph.py
+++ homework/chap2/phasegraph.py
@@ -3,21 +3,30 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-data = [[float(i) for i in l.split()] for l in open('Sitnikov.txt').readlines()]
-data = np.array(data).T
-index = data[0]
-indexes = [np.where(index==i)[0] for i in range(int(index[-1]))]
 
-z, vz, cj = data[4], data[7], data[-1]
+datafile = ['SitnikovCircular.txt', 'SitnikovEllipse.txt']
 
-fig = plt.figure()
-for i in range(len(indexes)):
-    lo = int(indexes[i][0])
-    hi = int(indexes[i][-1])+1
-    plt.plot(z[lo:hi], vz[lo:hi],label='$C_J=%f$' % cj[lo])
-plt.title('phase graph of Sitkinov problem')
-plt.xlabel('$z$',size=17)
-plt.ylabel('$dz/dt$',size=17)
-plt.legend()
-plt.savefig('phasegraph.png')
-plt.close()
+
+for orbit in [0, 1]:
+    data = [[float(i) for i in l.split()] for l in open(datafile[orbit]).readlines()]
+    data = np.array(data).T
+    index = data[0]
+    indexes = [np.where(index==i)[0] for i in range(int(index[-1])+1)]
+
+    z, vz = data[4], data[7]
+
+    fig = plt.figure()
+    for i in range(len(indexes)):
+        lo = int(indexes[i][0])
+        hi = int(indexes[i][-1])+1
+        if orbit == 0:
+            plt.plot(z[lo:hi], vz[lo:hi],label='$C_J=%f$' % data[-1][lo])
+        else:
+            plt.plot(z[lo:hi], vz[lo:hi])
+    plt.title('phase graph of Sitkinov problem')
+    plt.xlabel('$z$',size=17)
+    plt.ylabel('$dz/dt$',size=17)
+    if orbit == 0:
+        plt.legend()
+    plt.savefig(datafile[orbit][:-3]+'png')
+    plt.close()
