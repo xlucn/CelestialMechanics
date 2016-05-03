fd055b73eefbdd59e7f521124bd614ed75e30f67
OliverLew <oliver_lew@outlook.com>
2016-05-03 21:18:41 +0800

    add planets' physical parameters

--- include/Constants.h
+++ include/Constants.h
@@ -2,15 +2,80 @@
 #define _Const_H_
 
 #include "OrbitalEle_CoorVol_Trans.h"
+/**
+ * Physical constants
+ */
+#define Pi 3.14159265358979
+#define C 299792458
+#define G 6.673e-11
+#define AU 1.49597870691e11
 
-static const double Pi = 3.14159265358979;
-static const double C = 299792458;
-static const double G = 6.673e-11;
-static const double MSun = 1.98893e30;
-static const double MEarth = 5.97371e24;
-static const double AU = 1.49597870691e11;
+/**
+ * Time
+ */
+#define secondsperyear 31556952
 
-// the orbital elements below are based on the epoch = J2000.0
+
+/**
+ * Physical parameters of sun/planets in solar system
+ *
+ * RPlanet: radius / m
+ * MPlanet: mass / kg
+ * RhoPlanet: density / kg m^-3
+ * TrotPlanet: ratation period / hour
+ * TorbPlanet: orbital period / year
+ */
+
+#define RMercury 2440000
+#define RVenus 6051840
+#define REarth 6371010
+#define RMars 3389920
+#define RJupiter 69911000
+#define RSaturn 58232000
+#define RUranus 25362000
+#define RNeptune 24624000
+#define RPluto 1151000
+#define MSun 1.98893e30
+#define MMercury 3.302e23
+#define MVenus 4.8658e24
+#define MEarth 5.9736e24
+#define MMars 6.4185e23
+#define MJupiter 1.8986e27
+#define MSaturn 5.6846e26
+#define MUranus 8.6832e25
+#define MNeptune 1.0243e26
+#define MPluto 1.314e22
+#define RhoMercury 5.247
+#define RhoVenus 5.204
+#define RhoEarth 5.515
+#define RhoMars 3.9335
+#define RhoJupiter 1.326
+#define RhoSaturn 0.6873
+#define RhoUranus 1.318
+#define RhoNeptune 1.638
+#define RhoPluto 2.0
+#define TrotMercury
+#define TrotVenus
+#define TrotEarth
+#define TrotMars
+#define TrotJupiter
+#define TrotSaturn
+#define TrotUranus
+#define TrotNeptune
+#define TrotPluto
+#define TorbMercury 0.2408467
+#define TorbVenus 0.6151973
+#define TorbEarth 1.0000174
+#define TorbMars 1.8808476
+#define TorbJupiter 11.862615
+#define TorbSaturn 29.447498
+#define TorbUranus 84.016846
+#define TorbNeptune 164.79132
+#define TorbPluto 247.92065
+
+/**
+ * the orbital elements below are at the epoch = J2000.0
+ */
 static const OrbElem orbMercury = {0.38709893, 0.20563069, 7.00487, 48.33167, 77.45645, 252.25084};
 static const OrbElem orbVenus = {0.72333199, 0.00677323, 3.39471, 76.68069, 131.53298, 181.97973};
 static const OrbElem orbEarth = {1.00000011, 0.01671022, 0.00005, -11.26064, 102.94719, 100.46435};
